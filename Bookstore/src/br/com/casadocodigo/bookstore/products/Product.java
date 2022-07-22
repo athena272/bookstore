a5da3ca5b58bcce7c416f68f9ba63d8845e180fe
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-22 14:43:03 -0300

    Create Product.java

---
+++ Bookstore/src/br/com/casadocodigo/bookstore/products/Product.java
@@ -0,0 +1,7 @@
+package br.com.casadocodigo.bookstore.products;
+
+public interface Product extends Comparable<Product>{
+    //Methods are public and abstract by default
+    //public abstract double getValue();
+    double getValue();
+}
