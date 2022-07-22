f08fcdf3393818551c85fd15185101d700259eb7
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-22 14:43:12 -0300

    Create CalculadoraDeEstoque.java

---
+++ Bookstore/src/br/com/casadocodigo/bookstore/teste/CalculadoraDeEstoque.java
@@ -0,0 +1,14 @@
+package br.com.casadocodigo.bookstore.teste;
+
+public class CalculadoraDeEstoque {
+    public static void main(String[] args) {
+        double livroJava8 = 60;
+        double livroTDD = 60;
+
+        int numberInt = (int) livroJava8;
+
+        double soma = livroTDD + livroJava8;
+        System.out.println(soma);
+        System.out.println(numberInt);
+    }
+}
