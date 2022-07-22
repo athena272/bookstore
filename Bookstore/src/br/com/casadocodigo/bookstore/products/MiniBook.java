a1136584e9a30d24d16559357c997b2825282496
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-22 14:42:59 -0300

    Create MiniBook.java

---
+++ Bookstore/src/br/com/casadocodigo/bookstore/products/MiniBook.java
@@ -0,0 +1,9 @@
+package br.com.casadocodigo.bookstore.products;
+
+import br.com.casadocodigo.bookstore.Author;
+
+public class MiniBook extends Book {
+    public MiniBook(Author author) {
+        super(author);
+    }
+}
