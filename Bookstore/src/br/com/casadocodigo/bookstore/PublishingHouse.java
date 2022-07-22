4b22f6956e846075edf5da8b886725809a90855c
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-22 14:43:06 -0300

    Create PublishingHouse.java

---
+++ Bookstore/src/br/com/casadocodigo/bookstore/PublishingHouse.java
@@ -0,0 +1,32 @@
+package br.com.casadocodigo.bookstore;
+
+public class PublishingHouse {
+    private String fantasyName;
+    private String corporateName;
+    private String cnpj;
+
+    //Getters and Setters
+    public String getFantasyName() {
+        return fantasyName;
+    }
+
+    public void setFantasyName(String fantasyName) {
+        this.fantasyName = fantasyName;
+    }
+
+    public String getCorporateName() {
+        return corporateName;
+    }
+
+    public void setCorporateName(String corporateName) {
+        this.corporateName = corporateName;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
