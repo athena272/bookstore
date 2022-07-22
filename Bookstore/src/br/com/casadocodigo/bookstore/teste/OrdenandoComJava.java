eb5a33ae1f2c3f2b4b58c994420cf23ffdf38271
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-22 14:43:16 -0300

    Create OrdenandoComJava.java

---
+++ Bookstore/src/br/com/casadocodigo/bookstore/teste/OrdenandoComJava.java
@@ -0,0 +1,40 @@
+package br.com.casadocodigo.bookstore.teste;
+
+import br.com.casadocodigo.bookstore.Author;
+import br.com.casadocodigo.bookstore.products.PhysicalBook;
+import br.com.casadocodigo.bookstore.products.Ebook;
+import br.com.casadocodigo.bookstore.products.Product;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class OrdenandoComJava {
+    public static void main(String[] args) {
+        Author author = new Author();
+        author.setName("Rodrigo Turini");
+
+        PhysicalBook livroFisico = new PhysicalBook(author);
+        livroFisico.setName("Java 8 Prático");
+        livroFisico.setValue(59.90);
+
+        Ebook ebook = new Ebook(author);
+        ebook.setName("Java 8 Prático");
+        ebook.setValue(29.90);
+
+        List<Product> products = Arrays.asList(livroFisico, ebook);
+        Collections.sort(products);
+
+        for(Product product : products) {
+            System.out.println(product.getValue());
+        }
+        HashSet<String> set = new HashSet<String>();
+        set.add("Não há repitição");
+        set.add("Não há repitição");
+        set.add("Não há repitição");
+        set.add("Não há repitição");
+        set.add("Não há repitição");
+        System.out.println(set.size());
+    }
+}
