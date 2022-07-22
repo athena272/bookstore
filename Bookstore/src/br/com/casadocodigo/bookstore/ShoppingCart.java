5307acd7c5a588d0538013726a9739e7479583b1
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-22 14:43:07 -0300

    Create ShoppingCart.java

---
+++ Bookstore/src/br/com/casadocodigo/bookstore/ShoppingCart.java
@@ -0,0 +1,28 @@
+package br.com.casadocodigo.bookstore;
+
+import br.com.casadocodigo.bookstore.products.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private double total;
+    private List<Product> products;
+    public ShoppingCart() {
+        this.products = new ArrayList<Product>();
+    }
+    public void adiciona(Product product) {
+        this.products.add(product);
+    }
+    public void remove(int posicao) {
+        this.products.remove(posicao);
+    }
+
+    public double getTotal() {
+        return this.total;
+    }
+    public List<Product> getProdutos() {
+        return this.products;
+    }
+
+}
